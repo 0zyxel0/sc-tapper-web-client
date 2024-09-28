aa5121aafc916236d5976c3a351b3bb40becf05c
Metono <calvin.ongkingco@gmail.com>
2024-09-28 14:41:31 +0200

    - update emergencyContactDetail function

--- server/api/profile/emergencyContactDetails.ts
+++ server/api/profile/emergencyContactDetails.ts
@@ -1,17 +1,13 @@
 import axios from 'axios'
-import * as dotenv from 'dotenv'
-dotenv.config()
-const BASE_URL = process.env.BASE_URL
-
+const STRAPI_BASEURL = process.env.STRAPI_BASEURL;
 export default defineEventHandler(async (event) => {
   try {
     const body = await readBody(event)
     const myPayload = {    
       profile_publicid: body.profile_publicid
     }
-
     const result = await axios.post(
-      `${BASE_URL}/api/profile-contacts/profile-contact`,
+      `${STRAPI_BASEURL}/api/profile-contacts/profile-contact`,
       myPayload,
     )
     if (result) {
