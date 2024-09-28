ed5bfa46f39e0a188240e53e4103a226b2ffe2d9
Metono <calvin.ongkingco@gmail.com>
2024-09-28 02:22:57 +0200

    - updated try catch

--- pages/admin/profiles/add_profile.vue
+++ pages/admin/profiles/add_profile.vue
@@ -1,55 +1,55 @@
 <template>
-  <div>
-    <h1>Add Profile</h1>
+	<div>
+		<h1>Add Profile</h1>
 
-    <v-sheet class="mx-auto bg-deep-purple pa-12" width="750" rounded>
-      <v-form v-model="valid" ref="loginForm" lazy-validation>
-        <v-card>
-          <v-card-text>
-
-            <v-row>
-              <v-col cols="6">
-                <!-- <v-file-input v-model="image" />
+		<v-sheet class="mx-auto bg-deep-purple pa-12" width="750" rounded>
+			<v-form v-model="valid" ref="loginForm" lazy-validation>
+				<v-card>
+					<v-card-text>
+						<v-row>
+							<v-col cols="6">
+								<!-- <v-file-input v-model="image" />
                 <v-img :src="url" /> -->
 
-                <!-- <v-img :src="imagePreview"
+								<!-- <v-img :src="imagePreview"
                   lazy-src="https://picsum.photos/id/11/10/6" height="30vh"></v-img>
                 <v-file-input v-model="image" accept="image/png, image/jpeg, image/bmp" placeholder="Pick an image"
                   prepend-icon="mdi-camera" @change="selectImage" @click:clear="clearImagePreview()"
                   label="Image"></v-file-input> -->
 
-                <v-img :src="avatarImage ? imagePreviewURL : 'https://fakeimg.pl/400x400?text=Photo'" alt=""
-                  lazy-src="https://fakeimg.pl/400x400?text=Photo" style="max-width: 100%; object-fit: cover"
-                  height="40vh" />
+								<v-img
+									:src="avatarImage ? imagePreviewURL : 'https://fakeimg.pl/400x400?text=Photo'"
+									alt=""
+									lazy-src="https://fakeimg.pl/400x400?text=Photo"
+									style="max-width: 100%; object-fit: cover"
+									height="40vh"
+								/>
 
-                <v-file-input v-model="avatarImage" accept="image/png, image/jpeg, image/bmp" label="Image" required
-                  @change="onFileChange" @click:clear="clearImagePreview()"></v-file-input>
+								<v-file-input
+									v-model="avatarImage"
+									accept="image/png, image/jpeg, image/bmp"
+									label="Image"
+									required
+									@change="onFileChange"
+									@click:clear="clearImagePreview()"
+								></v-file-input>
+							</v-col>
+							<v-col cols="6">
+								<v-text-field v-model="student_no" :rules="rules.studno" label="Student No"></v-text-field>
+								<v-text-field v-model="last_name" :rules="rules.lname" label="Last name"></v-text-field>
+								<v-text-field v-model="first_name" :rules="rules.fname" label="First name"></v-text-field>
+								<v-text-field v-model="middle_name" :rules="rules.mname" label="Middle name"></v-text-field>
+							</v-col>
+						</v-row>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn class="mt-4" color="primary" variant="outlined" size="large" block @click.prevent="onSubmit">Submit</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-form>
+		</v-sheet>
 
-
-
-
-
-              </v-col>
-              <v-col cols="6">
-                <v-text-field v-model="student_no" :rules="rules.studno" label="Student No"></v-text-field>
-                <v-text-field v-model="last_name" :rules="rules.lname" label="Last name"></v-text-field>
-                <v-text-field v-model="first_name" :rules="rules.fname" label="First name"></v-text-field>
-                <v-text-field v-model="middle_name" :rules="rules.mname" label="Middle name"></v-text-field>
-
-              </v-col>
-            </v-row>
-
-
-          </v-card-text>
-          <v-card-actions>
-            <v-btn class="mt-4" color="primary" variant="outlined" size="large" block
-              @click.prevent="onSubmit">Submit</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-form>
-    </v-sheet>
-
-    <!-- <v-card>
+		<!-- <v-card>
       <v-card-text>
         <label>Enter Company Name</label>
         <input type="text" v-model="name"/>
@@ -63,13 +63,13 @@
         <v-btn @click="submit">Submit</v-btn>
       </v-card-text>
     </v-card> -->
-  </div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { useUseUpload } from '~/composables/useUpload';
+import { useUseUpload } from "~/composables/useUpload";
 //@ts-ignore
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const { uploadImage } = useUseUpload();
 
@@ -83,7 +83,7 @@ const imagePreviewURL = ref(null);
 const avatarImage = ref(null);
 
 const files = ref([]);
-const name = ref('');
+const name = ref("");
 const uploadResult = ref(null);
 const newImageName = ref(null);
 const publicID = ref("");
@@ -94,76 +94,78 @@ const publicID = ref("");
 // const mnameRules = [(v) => !!v || 'Student no is required'];
 
 const rules = ref({
-  studno: [(value: string) => !!value || "Student no is requred"],
-  lname: [(value: string) => !!value || "Last name is requred"],
-  fname: [(value: string) => !!value || "First name is requred"],
-  mname: [(value: string) => !!value || "Middle name is requred"],
-})
+	studno: [(value: string) => !!value || "Student no is requred"],
+	lname: [(value: string) => !!value || "Last name is requred"],
+	fname: [(value: string) => !!value || "First name is requred"],
+	mname: [(value: string) => !!value || "Middle name is requred"],
+});
 const loginForm = ref(null);
 const valid = ref(true);
 
 async function onFileChange(event) {
-  // Generate UUID
-  const genId = uuidv4();
+	// Generate UUID
+	const genId = uuidv4();
 
-  const image = event.target.files[0];
-  const originalName = event.target.files[0].name;
-  const ext = originalName.split(".").pop();
-  const blob = image.slice(0, image.size);
-  publicID.value = genId;
-  newImageName.value = new File([blob], `${genId}.${ext}`, { type: `${image.type}` });
+	const image = event.target.files[0];
+	const originalName = event.target.files[0].name;
+	const ext = originalName.split(".").pop();
+	const blob = image.slice(0, image.size);
+	publicID.value = genId;
+	newImageName.value = new File([blob], `${genId}.${ext}`, { type: `${image.type}` });
 
-  if (image) {
-    imagePreviewURL.value = URL.createObjectURL(image);
-    URL.revokeObjectURL(image);
-  } else {
-    imagePreviewURL.value = null;
-  }
+	if (image) {
+		imagePreviewURL.value = URL.createObjectURL(image);
+		URL.revokeObjectURL(image);
+	} else {
+		imagePreviewURL.value = null;
+	}
 
-  console.log("Current name: ", image);
-  console.log("New name: ", newImageName.value);
-  console.log("Extension: ", ext);
+	console.log("Current name: ", image);
+	console.log("New name: ", newImageName.value);
+	console.log("Extension: ", ext);
 }
 
 async function clearImagePreview() {
-  imagePreviewURL.value = "";
+	imagePreviewURL.value = "";
 }
 
-
 async function onSubmit() {
-  const { valid, errors } = await loginForm.value?.validate();
-  if (valid) {
-    if (newImageName.value) {
-      try {
-        console.log()
-        const imageUploadResult = await uploadImage(newImageName.value);
-        console.log(imageUploadResult);
-        console.log("Success", imageUploadResult[0].name);
-        console.log("Image Url:", imageUploadResult[0].url);
-        console.log("Hash: ", imageUploadResult[0].hash);
-        const payload = {
-          student_no: student_no.value,
-          last_name: last_name.value,
-          first_name: first_name.value,
-          middle_name: middle_name.value,
-          publicid: uploadResult.value[0].hash,
-          image_url: uploadResult.value[0].url
-        }
-        console.log("payload ",payload);
-        await $fetch(`/api/createProfile`, {
-          method: 'POST',
-          body: payload,
-        });
-        loginForm.value?.reset();
-        console.log("Successfully Submited!");
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      console.error("No file selected");
-    }
-  }
-
+	const { valid, errors } = await loginForm.value?.validate();
+	if (valid) {
+		if (newImageName.value) {
+			try {
+				console.log();
+				const imageUploadResult = await uploadImage(newImageName.value);
+				console.log(imageUploadResult);
+				console.log("Success", imageUploadResult[0].name);
+				console.log("Image Url:", imageUploadResult[0].url);
+				console.log("Hash: ", imageUploadResult[0].hash);
+				if (imageUploadResult) {
+					const payload = {
+						student_no: student_no.value,
+						last_name: last_name.value,
+						first_name: first_name.value,
+						middle_name: middle_name.value,
+						publicid: imageUploadResult[0].hash,
+						image_url: imageUploadResult[0].url,
+					};
+					console.log("payload ", payload);
+					const profileResult = await $fetch(`/api/createProfile`, {
+						method: "POST",
+						body: payload,
+					});
+          if(profileResult){
+            loginForm.value?.reset();
+            console.log("Successfully Submited!");
+          }					
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		} else {
+			console.error("No file selected");
+		}
+	}
 }
 
 // async function selectImage(e) {
@@ -195,16 +197,13 @@ async function onSubmit() {
 //   }
 // }
 
-
-
 const handleFileSelection = (event) => {
-  // imagePreviewURL.value = event.target.files[0];
-  //console.log(uploadedFiles);
-  // for ( let i = 0; i < uploadedFiles.lenght; i++) {
-  //   files.value.push( uploadedFiles[i] );
-
-  // }
-}
+	// imagePreviewURL.value = event.target.files[0];
+	//console.log(uploadedFiles);
+	// for ( let i = 0; i < uploadedFiles.lenght; i++) {
+	//   files.value.push( uploadedFiles[i] );
+	// }
+};
 
 // async function submit() {
 //   let formData = new FormData();
@@ -222,11 +221,6 @@ const handleFileSelection = (event) => {
 //     body: formData
 //   },)
 // }
-
-
-
-
-
 </script>
 
 <style></style>
